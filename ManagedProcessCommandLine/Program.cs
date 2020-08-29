db22b77d06ac11b9b52b1653ecfa58b98f34c07d
Andrew Heinlein <heinlein@dciartform.com>
2020-08-28 22:09:22 -0500

    converted the C++ code to C# and added it to a new project.

---
+++ ManagedProcessCommandLine/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ManagedProcessCommandLine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Managed ProcessCommandLine");
+			Console.WriteLine("------------------------\n");
+
+			foreach (var p in Process.GetProcesses())
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine($"{p.ProcessName} ({p.Id}):");
+				Console.ResetColor();
+
+				var rc = ProcessCommandLine.Retrieve(p, out var cl);
+				if (0 == rc)
+				{
+					var cmdLineArray = ProcessCommandLine.CommandLineToArgs(cl);
+					cmdLineArray.Select((x, i) => $"\targ {i}:\t{x}").ToList().ForEach(Console.WriteLine);
+				}
+				else
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"\tUnable to get command line ({rc}): {ProcessCommandLine.ErrorToString(rc)}");
+					Console.ResetColor();
+				}
+			}
+
+			Console.Write("\nPress any key to quit ...");
+			Console.ReadKey(true);
+		}
+	}
+}
